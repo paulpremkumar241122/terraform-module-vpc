20f5c7a3371fa09d62ac0bb8699af4ca6dd0d34a
Paul Prem Kumar <paul.prem5@gmail.com>
2024-01-23 03:05:05 +0530

    vpc peering with default vpc

--- main.tf
+++ main.tf
@@ -21,3 +21,13 @@ module "subnets" {
   env    = var.env
   tags   = var.tags
 }
+
+
+
+resource "aws_vpc_peering_connection" "peer" {
+  peer_vpc_id   = aws_vpc.main.id
+  vpc_id        = var.defaul_vpc_id
+  auto_accept   = true
+}
+
+
