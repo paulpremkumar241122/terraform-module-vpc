52e3da6c1d7c79a1cb57b7082081ff7062064033
Paul Prem Kumar <paul.prem5@gmail.com>
2024-01-23 03:10:07 +0530

    vpc peering with default vpc

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ module "subnets" {
 
 resource "aws_vpc_peering_connection" "peer" {
   peer_vpc_id   = aws_vpc.main.id
-  vpc_id        = var.defaul_vpc_id
+  vpc_id        = var.default_vpc_id
   auto_accept   = true
 }
 
--- variables.tf
+++ variables.tf
@@ -6,4 +6,4 @@ variable "az" {
   default = [ "us-east-1a", "us-east-1b" ]
 }
 
-variable "defaul_vpc_id" {}
+variable "default_vpc_id" {}
